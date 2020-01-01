31a794ae016e15c6530f59f8ba1a03d39bf1fca8
wzs <971742937@qq.com>
2020-01-01 21:21:06 +0800

    finish dense and now can train

--- MoE_Densev1.py
+++ MoE_Densev1.py
@@ -15,21 +15,22 @@ from DenseMoE import DenseMoE
 from scipy.io import savemat
 import scipy.io as scio
 from keras.utils import to_categorical
-from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
+from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, EarlyStopping,TensorBoard
+import datetime
 def main():
     n_data = 50000
     n_inp_dim = 1024
     n_hid_dim = 10
     which_model='dense' # 'dense' or 'moe'
-    batch_size=8
-    epochs=500
+    batch_size=4
+    epochs=20
     ##read files
     train_x = scio.loadmat(r'C:\Users\wzs\Desktop\DFCN\data_feature_train.mat')['train']
     test_x = scio.loadmat(r'C:\Users\wzs\Desktop\DFCN\data_feature_test.mat')['test']
     f = h5py.File(r'C:\Users\wzs\Desktop\DFCN\train_dataset.h5','r')                             
     train_y_orig = f['train_labels'][:] 
     train_y=to_categorical(train_y_orig) 
-    train_y=train_y*10
+    train_y=train_y
     f = h5py.File(r'C:\Users\wzs\Desktop\DFCN\test_dataset.h5','r')                            
     test_y_orig = f['test_labels'][:] 
     test_y=to_categorical(test_y_orig)
@@ -39,24 +40,35 @@ def main():
     
     if which_model=='moe':
         n_experts = 2
-        hidden = DenseMoE(n_hid_dim, n_experts, expert_activation='relu', gating_activation='softmax')(inputs)
+        hidden = DenseMoE(n_hid_dim, n_experts, expert_activation='softmax', gating_activation='softmax')(inputs)
     elif which_model=='dense':
-        hidden = Dense(n_hid_dim, activation='relu')(inputs)
+        hidden = Dense(n_hid_dim, activation='softmax',kernel_initializer='RandomUniform')(inputs)
     
     model = Model(inputs=inputs, outputs=hidden)
     print("Model created")
     model.summary()
+#    log_dir=".\logs\fit\" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+#    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+    tbCallBack = TensorBoard(log_dir='.\logs\moe_2', 
+                 histogram_freq=0,
+#                  batch_size=32,     
+                 write_graph=False,  
+                 write_grads=False, 
+                 write_images=False,
+                 embeddings_freq=0, 
+                 embeddings_layer_names=None, 
+                 embeddings_metadata=None)
     ##train model
-    opt = keras.optimizers.SGD(lr=1e-7, decay=1e-5)
-    model.load_weights("weights/Denlayer.h5")
+    opt = keras.optimizers.SGD(lr=1e-4, decay=1e-7)
+    model.load_weights("weights/Denlayer_moe_2.h5")
     print("Model loaded.")
 
     lr_reducer= ReduceLROnPlateau(monitor='val_loss', factor=np.sqrt(0.1),
                                       cooldown=0, patience=10, min_lr=0.5e-6)
     early_stopper= EarlyStopping(monitor='val_acc', min_delta=0.0001, patience=20)
-    model_checkpoint= ModelCheckpoint("weights/Denlayer.h5", monitor="val_acc", save_best_only=True,
+    model_checkpoint= ModelCheckpoint("weights/Denlayer_moe_2.h5", monitor="val_acc", save_best_only=True,
                                     save_weights_only=False)
-    callbacks=[lr_reducer, early_stopper, model_checkpoint]
+    callbacks=[lr_reducer, early_stopper, model_checkpoint,tbCallBack]
 
     model.compile(loss='categorical_crossentropy',
                   optimizer=opt,
